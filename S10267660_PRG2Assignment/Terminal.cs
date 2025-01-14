1fadd4becdce9bf856211d4ced61969144878b15
Jovan Zheng Shuan Neo <s10267660@connect.np.edu.sg>
2025-01-14 22:40:46 +0800

    added methods to terminal.cs Edited constructors for terminal.cs and airline.cs

--- S10267660_PRG2Assignment/Terminal.cs
+++ S10267660_PRG2Assignment/Terminal.cs
@@ -15,22 +15,67 @@ namespace S10267660_PRG2Assignment
      class Terminal
     {
         public string TerminalName { get; set; }
-        public Dictionary<string, Airline> Airlines { get; set; }
-        public Dictionary<string,Flight > Flights { get; set; }
-        public Dictionary<string,BoardingGate>BoardingGates { get; set; }
-        public Dictionary<string,double>GateFees { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; } = new Dictionary<string, Airline>();
+        public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; } = new Dictionary<string, BoardingGate>();
+        public Dictionary<string, double> GateFees { get; set; } = new Dictionary<string, double>();
 
-        public Terminal(string TN, Dictionary<string,Airline>A, Dictionary<string, Flight> F, Dictionary<string, BoardingGate> BG, Dictionary<string, double> GF)
+        public Terminal(string TN)
         {
             TerminalName = TN;
-            Airlines = A;
-            Flights = F;
-            BoardingGates = BG;
-            GateFees = GF;
+        }
+
+        public bool AddAirline(Airline airline)
+        {
+            try
+            {
+                Airlines[airline.Code] = airline;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not add airline!");
+                return false;
+            }
+        }
+
+        public bool AddBoardingGate(BoardingGate boardinggate)
+        {
+            try
+            {
+                BoardingGates[boardinggate.GateName] = boardinggate;
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not add boarding gate!");
+                return false;
+            }
+        }
+
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+            foreach (Airline airlineInfo in Airlines.Values)
+            {
+                if (airlineInfo.Flights.ContainsKey(flight.FlightNumber))
+                {
+                    return airlineInfo;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return null;
+        }
+
+        public void PrintAirlineFees()
+        {
+            throw new NotImplementedException();
         }
         public override string ToString()
         {
-            return "Terminal Name: " + TerminalName + " Airlines: " + Airlines + " Flights: " + Flights + " Boarding Gates " + BoardingGates + " Gate Fees: " + GateFees;
+            return "Terminal Name: " + TerminalName + " Airlines: " + Airlines.Values + " Flights: " + Flights.Values + " Boarding Gates " + BoardingGates.Values + " Gate Fees: " + GateFees.Values;
 
         }
     }
